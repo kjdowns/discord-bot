39463de27e00df849137f87f73781112e662b730
kjdowns <kevin.downs04@gmail.com>
2020-11-09 19:22:14 -0500

    add comments

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// Imports
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// Setup
 require('dotenv').config();
 client.login(process.env.BOT_TOKEN);
 
+// Actions
 client.on('ready', () => console.log("Ready!"));
+
+// Functions
