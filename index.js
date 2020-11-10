df6a2bde50cbbe22f088adb9abf1caa431036d78
kjdowns <kevin.downs04@gmail.com>
2020-11-09 19:30:14 -0500

    stub out replyMessage func and add replies array

--- index.js
+++ index.js
@@ -2,11 +2,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// Variables
+const replies = [
+    'Aye Aye, Captain! 🦀🦀🦀',
+    `I can't hear youuuuu! 🦀🦀🦀 `,
+    `Who lives in a 🍍 under the sea?`
+];
+
 // Setup
 require('dotenv').config();
 client.login(process.env.BOT_TOKEN);
 
 // Actions
 client.on('ready', () => console.log("Ready!"));
+client.on('message', replyMessage);
 
 // Functions
+function replyMessage(msg){
+    if (msg.channel.id === '' && msg.content === "arrr you ready kids?!") {
+        
+    }
+}
